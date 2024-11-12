e294160f1723d5d65dc00e1cc131032ff91024da
sBubshait <s.bobshit@gmail.com>
2024-11-12 07:26:41 +0000

    Fix Bug in Web to prevent crashing project when token is not provided on build

--- web/app/lib/serverTokens.ts
+++ web/app/lib/serverTokens.ts
@@ -1,12 +1,9 @@
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const JWT_SECRET = process.env.JWT_SECRET!;
-if (!JWT_SECRET) {
-  throw new Error('SERVER_SECRET environment variable is required');
-}
 
 export function generateServerToken(id: string): string {
-  return jwt.sign({ source: 'website', id }, JWT_SECRET, { expiresIn: '5m' });
+  return jwt.sign({ source: "website", id }, JWT_SECRET, { expiresIn: "5m" });
 }
 
 export function verifyServerToken(token: string): boolean {
